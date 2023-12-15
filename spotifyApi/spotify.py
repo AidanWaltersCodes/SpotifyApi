3b94fda2d36b151e7ffb27ceaf778f5a0947e75e
AidanWalters <aidanpwalters@gmail.com>
2023-12-14 19:01:23 -0500

    Imported os

--- spotifyApi/spotify.py
+++ spotifyApi/spotify.py
@@ -6,9 +6,13 @@ import spotipy
 #dotenv will let me put my tokens in a file that will be ignored by git
 from dotenv import load_dotenv
 
+#This will allow me to grab my keys from the env file
+import os 
+
 #Load variables created in .env file
 load_dotenv()
 
 
-CLIENT_ID=your_client_id
-CLIENT_SECRET=your_client_secret
+CLIENT_ID = os.getenv("CLIENT_ID")
+CLIENT_SECRET = os.getenv("CLIENT_SECRET")
+
